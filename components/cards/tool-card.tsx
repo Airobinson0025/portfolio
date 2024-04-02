b711aca54bebbd8c87af61b00ea42f5532236445
Aaron Robinsom <airobinson0025@gmail.com>
2024-04-02 12:10:21 -0400

    tools section added

--- components/cards/tool-card.tsx
+++ components/cards/tool-card.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { IconType } from 'react-icons/lib'
 
-type Props = {}
+type Tool = {
+  icon: IconType
+  name: string
+  proficiency: string
+}
 
-const ToolCard = (props: Props) => {
+type ToolCardProps = {
+  tool: Tool
+}
+
+const ToolCard: React.FC<ToolCardProps> = ({ tool }) => {
+  const Icon = tool.icon
   return (
-    <div>tool-card</div>
+    <div className='flex flex-col items-center rounded-md py-6 bg-secondary shadow-lg'>
+      <Icon size={50} />
+      <h3 className='text-lg font-bold mt-2'>{tool.name}</h3>
+    </div>
   )
 }
 
--- components/global/tools.tsx
+++ components/global/tools.tsx
@@ -1,14 +1,102 @@
 "use client"
 import React from 'react'
+import { IconType } from 'react-icons/lib'
+import ToolCard from '../cards/tool-card'
+import { FaHtml5, FaReact } from 'react-icons/fa'
+import { SiDjango, SiJavascript, SiNextdotjs, SiPostgresql, SiPrisma, SiPython, SiTailwindcss, SiTypescript } from 'react-icons/si'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs'
 
-type Props = {}
+
+
+
+type Tool = {
+    icon: IconType
+    name: string
+    proficiency: string
+}
 
 const Tools: React.FC = () => {
+  const toolsArray: Tool[] = [
+    {
+      icon: FaHtml5,
+      name: 'HTML5',
+      proficiency: 'profiecient'
+    },
+    {
+      icon: SiTailwindcss,
+      name: 'Tailwind CSS',
+      proficiency: 'profiecient'
+    },
+    {
+      icon: SiJavascript,
+      name: 'JavaScript',
+      proficiency: 'profiecient'
+    },
+    {
+      icon: SiTypescript,
+      name: 'TypeScript',
+      proficiency: 'profiecient'
+    },
+    {
+      icon: FaReact,
+      name: 'React.js',
+      proficiency: 'profiecient'
+    },
+    {
+      icon: SiNextdotjs,
+      name: 'Next.js',
+      proficiency: 'profiecient'
+    },
+    {
+      icon: SiDjango,
+      name: 'Django',
+      proficiency: 'learning'
+    },
+    {
+      icon: SiPython,
+      name: 'Python',
+      proficiency: 'learning'
+    },
+    {
+      icon: SiPostgresql,
+      name: 'PostgreSQL',
+      proficiency: 'profiecient'
+    },
+    {
+      icon: SiPrisma,
+      name: 'Prisma',
+      proficiency: 'profiecient'
+    }
+  ]
+
+  const proficientTools = toolsArray.filter(tool => tool.proficiency === 'profiecient')
+  // console.log(proficientTools)
+  const learningTools = toolsArray.filter(tool => tool.proficiency === 'learning')
+  // console.log(learningTools)
+
   return (
-    <section className='px-8 mt-32 text-center'>
-        <div>
-            <h1>Tools and Technology</h1>
-        </div>
+    <section className='mt-32 flex flex-col items-center'>
+        <h1 className='text-center mb-14'>Tools and Technology</h1>
+        <Tabs defaultValue='proficient' className='w-[400px] md:w-[700px] lg:w-[850px] h-[750px] md:h-[600px]'>
+          <TabsList className='grid w-full grid-cols-2 shadow-sm'>
+            <TabsTrigger value='proficient'>Proficient</TabsTrigger>
+            <TabsTrigger value='learning'>Learning</TabsTrigger>
+          </TabsList>
+          <TabsContent value='proficient' className='mt-8'>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-12'>
+              {proficientTools.map((tool, index) => (
+                <ToolCard key={index} tool={tool} />
+              ))}
+            </div>
+          </TabsContent>
+          <TabsContent value='learning' className='mt-8'>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-12'>
+              {learningTools.map((tool, index) => (
+                <ToolCard key={index} tool={tool} />
+              ))}
+            </div>
+          </TabsContent>
+        </Tabs>
     </section>
   )
 }
